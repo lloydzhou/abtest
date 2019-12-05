a3c5369d21375a6892a8dc3256d3729cc2566314
lloydzhou <lloydzhou@qq.com>
2019-12-05 12:44:18 +0800

    将添加层逻辑放到redis lua里面 #11

--- lua/add-layer.lua
+++ lua/add-layer.lua
@@ -1,7 +1,7 @@
 
 local exists = redis.call("sismember", "layers", ARGV[1])
-if exists == 0 then
-    return 0
+if exists == 1 then
+    return {-1, "layer exists"}
 end
-return redis.call("sadd", "layers", ARGV[1])
+return {redis.call("sadd", "layers", ARGV[1]), "add layer success"}
 
