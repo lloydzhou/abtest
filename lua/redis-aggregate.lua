0b306935c63ac923650d46f3a44bb16ed969eab3
lloydzhou <lloydzhou@qq.com>
2019-12-05 21:15:54 +0800

    增加计算均值和方差 #10

--- lua/redis-aggregate.lua
+++ lua/redis-aggregate.lua
@@ -1,10 +1,15 @@
 local count = 0
 local sum = 0
+local mean = 0
+local s = 0
+local std = 0
 local min = 0
 local max = 0
 local cursor = "0"
 local result = nil
 local data = nil
+
+-- S(n) = S(n-1) + (x(n) - E(n-1))(x(n) - E(n-))
 
 repeat 
     result = redis.call("zscan", KEYS[1], cursor, "count", 100)
@@ -15,6 +20,8 @@ repeat
 
         count = count + 1
         sum = sum + score
+        s = s + 1.0 * (count - 1) / count * (score - mean) * (score - mean)
+        mean = mean + 1.0 * (score - mean) / count
 
         if i == 1 then
             min = score
@@ -26,6 +33,11 @@ repeat
     end
 until cursor == "0"
 
+std = math.sqrt(s / (count - 1))
+
 return {
-    count, sum, min, max
+    tostring(count), tostring(sum),
+    tostring(min), tostring(max),
+    tostring(mean), tostring(std)
 }
+
