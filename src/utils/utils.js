7f2dec4d7072fbaba1f97a291c190a6063e3aaae
lloydzhou <lloydzhou@qq.com>
2019-12-11 11:17:26 +0800

    重新计算假设检验统计指标 #10

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -37,4 +37,25 @@ export function ZScore(mean1, std1, n1, mean2, std2, n2) {
   return (mean1 - mean2) / Math.sqrt(std1 * std1 / n1 + std2 * std2 / n2)
   //return (mean1 - mean2) / (Math.sqrt(std1 * std1 / (n1||1) + std2 * std2 / (n2||1)) ||1)
 }
+export function realMeanStd (mean, std, user, uv) {
+    // 服务端统计的实际是uv的均值和方差，以及target的均值和方差
+    // 实际上统计需要的均值方差是target的总值针对uv计算出的均值和方差
+    // -- S(n) = S(n-1) + (x(n) - E(n-1))(x(n) - E(n-1))
+    // count = count + 1
+    // s = s + 1.0 * (count - 1) / count * (score - mean) * (score - mean)
+    // mean = mean + 1.0 * (score - mean) / count
+    // console.log(mean, std, user, uv)
+    let real_mean = mean
+    let real_s = (std * std) * (user - 1)
+    for (let i = user; i < uv; i++) {
+      const score = 0;
+      const count = i + 1
+      real_s = real_s + (count - 1) / count * (score - real_mean) * (score - real_mean);
+      real_mean = real_mean + (score - real_mean) / count;
+      // console.log(real_mean, real_s, i)
+    }
+    const real_std = Math.sqrt(real_s / (uv - 1))
+    // console.log(real_mean, real_std, user, uv, mean * user / uv)
+    return [real_mean, real_std, uv]
+  }
 
